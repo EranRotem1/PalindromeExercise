0383073d2614ec4d0970391491d39737da1336c0
Eran Rotem <eranrotem123@gmail.com>
2023-02-27 12:59:39 -0600

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public static bool IsAPalindrome(string word)
+        {
+            word = word.ToLower();
+            return word.SequenceEqual(word.Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,16 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("elite", false)]
+        [InlineData("elile", true)]
+        public void IsPalindromeTest(string word, bool expected)
         {
-
+            var actual = WordSmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
